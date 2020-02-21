4c2a362a4979740f4074db949790353ab4ad1563
FilipTota <58394415+FilipTota@users.noreply.github.com>
2020-02-21 15:12:58 +0100

    Updates

---
+++ precache-manifest.0a9f31737a4e3cf9e7759be2d5c181e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32211f24615d35a53738bca7399e8b8",
+    "url": "/formvalidation/index.html"
+  },
+  {
+    "revision": "c7526d0eabbb4b4828ed",
+    "url": "/formvalidation/static/css/main.fa64f77e.chunk.css"
+  },
+  {
+    "revision": "cdca927bbe7ec68abff3",
+    "url": "/formvalidation/static/js/2.c92da8c2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/formvalidation/static/js/2.c92da8c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7526d0eabbb4b4828ed",
+    "url": "/formvalidation/static/js/main.024a4797.chunk.js"
+  },
+  {
+    "revision": "9f63004ab9b6e8a82e62",
+    "url": "/formvalidation/static/js/runtime-main.154be1ee.js"
+  }
+]);
